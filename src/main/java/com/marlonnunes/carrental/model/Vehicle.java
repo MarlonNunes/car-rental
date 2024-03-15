ad91e890c8f200fcf73c8f6781c433200e38abbc
Marlon Nunes <marlon@alfamidia.com.br>
2024-03-15 01:06:08 -0300

    FEAT: created vehicle model and repository

--- src/main/java/com/marlonnunes/carrental/model/Vehicle.java
+++ src/main/java/com/marlonnunes/carrental/model/Vehicle.java
@@ -1,5 +1,21 @@
 package com.marlonnunes.carrental.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
 
+@Entity
+@Data
 public class Vehicle {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String numberPlate;
+
+    private String model;
+
+    private String make;
+
+    private String color;
 }
