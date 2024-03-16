c10dc368e73d97bf33126929f9d2ac6049c73b19
Marlon Nunes <marlon@alfamidia.com.br>
2024-03-15 21:26:27 -0300

    FEAT: created user model and repository

---
+++ src/main/java/com/marlonnunes/carrental/model/User.java
@@ -0,0 +1,19 @@
+package com.marlonnunes.carrental.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column(unique = true)
+    private String cpf;
+}
