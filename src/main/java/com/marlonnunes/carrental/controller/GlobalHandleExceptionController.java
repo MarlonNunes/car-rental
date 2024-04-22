0944331e103e1c0c20b19b79ec6bdd6f9c5e9629
Marlon Nunes <marlonnunes869@gmail.com>
2024-04-22 00:03:42 -0300

    feat: improving error messages

--- src/main/java/com/marlonnunes/carrental/controller/GlobalHandleExceptionController.java
+++ src/main/java/com/marlonnunes/carrental/controller/GlobalHandleExceptionController.java
@@ -1,5 +1,6 @@
 package com.marlonnunes.carrental.controller;
 
+import com.fasterxml.jackson.databind.exc.MismatchedInputException;
 import com.marlonnunes.carrental.dto.commons.ErrorResponseDTO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,7 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -67,6 +69,18 @@ public class GlobalHandleExceptionController {
     public ResponseEntity<ErrorResponseDTO> handleMissingServletRequestParameterException(NoResourceFoundException e){
         log.error("an error has ocurred", e);
         return new ResponseEntity<>(ErrorResponseDTO.simpleError(this.getMessage("controller.global-handle-exception.no-resource-found")), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MismatchedInputException.class)
+    public ResponseEntity<ErrorResponseDTO> handleMismatchedInputException(MismatchedInputException e){
+        log.error("an error has ocurred", e);
+        return new ResponseEntity<>(ErrorResponseDTO.simpleError(this.getMessage("controller.global-handle-exception.mismatched-input-exception")), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorResponseDTO> handleMismatchedInputException(HttpMessageNotReadableException e){
+        log.error("an error has ocurred", e);
+        return new ResponseEntity<>(ErrorResponseDTO.simpleError(this.getMessage("controller.global-handle-exception.mismatched-input-exception")), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(Exception.class)
--- src/main/java/com/marlonnunes/carrental/dto/keycloak/RoleDTO.java
+++
@@ -1,9 +0,0 @@
-package com.marlonnunes.carrental.dto.keycloak;
-
-public record RoleDTO(
-        String id,
-        String name,
-        String description,
-        boolean composite
-) {
-}
